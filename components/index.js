0f31656a998dba7e3e6f4ed2a25417c4cb4aed91
Piyush Jha <jhapiyush06@gmail.com>
2022-04-25 11:48:14 +0530

    Made First react native app

---
+++ components/index.js
@@ -0,0 +1,24 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo , People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    CircleButton,
+    RectButton, 
+    SubInfo,
+    People, 
+    EndDate, 
+    EthPrice, 
+    NFTTitle,
+    DetailsBid,
+    DetailsDesc,
+}
+
+
